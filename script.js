dc0ec9dd62a3662498e680b34b2c1bcad681f162
Henrydev-bit <erasmo.vrd@gmail.com>
2026-02-19 18:04:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let contador = 0;
 
+mostrar = document.getElementById("mostrar")
+let vezescliques = 2
+let up = document.getElementById("up");
+let preco = 30
+let valorClique = 1
 let botao = document.getElementById("botao");
 let texto = document.getElementById("texto");
+let score = 0;
 
 botao.addEventListener("click", function() {
-    contador++;
-    texto.textContent = `Cliques ${contador}`;
+    score = score + valorClique
+    texto.textContent = `score ${score}`
 });
+
+up.addEventListener("click", function() {
+  if (score >= preco)  {
+      valorClique = valorClique + 1 
+      score = score - preco
+      texto.textContent = `score ${score}`
+      preco = preco*3
+      vezescliques = vezescliques +1     
+      mostrar.textContent = `cliques vezes: ${valorClique}`
+      up.textContent =  `${vezescliques}x clique preço:${preco}`
+      
+      
+  }
+  });
